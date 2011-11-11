34cfbae700d5947d5f96c19366b5ac6400c040b1
Chris Clarke <clarketc@new-host-5.home>
2011-11-11 11:21:44 -0500

    Initial Commit

---
+++ mileage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mileage
+//
+//  Created by Chris Clarke on 11/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
